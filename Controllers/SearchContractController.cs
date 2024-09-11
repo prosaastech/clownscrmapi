01d23ca866dd960fbc0947e0795707ef184558da
faisalmotan <faisal.motan3@gmail.com>
2024-09-11 21:20:41 +0500

    Search changes in query

--- Controllers/SearchContractController.cs
+++ Controllers/SearchContractController.cs
@@ -30,69 +30,7 @@ namespace ClownsCRMAPI.Controllers
                 page = page == 0 ? 1 : page;
                 int branchId = TokenHelper.GetBranchId(HttpContext);
                 int companyId = TokenHelper.GetCompanyId(HttpContext);
-
-                //var query = (from customer in _context.CustomerInfos
-
-                //                 // Left join with ContractEventInfos
-                //             join eventInfo in _context.ContractEventInfos
-                //             on customer.CustomerId equals (int?)eventInfo.CustomerId into customerEvents
-                //             from eventInfo in customerEvents.DefaultIfEmpty()
-
-                //                 // Left join with ContractTimeTeamInfos (using composite keys)
-                //             join timeTeamInfo in _context.ContractTimeTeamInfos
-                //             on new { CustomerId = (int?)customer.CustomerId, ContractId = (int?)eventInfo.ContractId, BranchId = (int?)customer.BranchId, CompanyId = (int?)customer.CompanyId }
-                //             equals new { CustomerId = (int?)timeTeamInfo.CustomerId, ContractId = (int?)timeTeamInfo.ContractId, BranchId = (int?)timeTeamInfo.BranchId, CompanyId = (int?)timeTeamInfo.CompanyId } into eventTimes
-                //             from timeTeamInfo in eventTimes.DefaultIfEmpty()
-
-                //                 // Left join with States
-                //             join state in _context.States
-                //             on customer.StateId equals (int?)state.StateId into stateInfo
-                //             from state in stateInfo.DefaultIfEmpty()
-
-                //                 // Left join with Contract_PackageInfo based on CustomerId and ContractId
-                //             join contractPackage in _context.ContractPackageInfos
-                //             on new { CustomerId = (int?)customer.CustomerId, ContractId = (int?)timeTeamInfo.ContractId }
-                //             equals new { CustomerId = (int?)contractPackage.CustomerId, ContractId = (int?)contractPackage.ContractId } into contractPackages
-                //             from contractPackage in contractPackages.DefaultIfEmpty()
-
-                //                 // Left join with PartyPackage based on PartyPackageId from Contract_PackageInfo
-                //             join partyPackage in _context.PartyPackages
-                //             on (int?)contractPackage.PartyPackageId equals (int?)partyPackage.PartyPackageId into partyPackageInfo
-                //             from partyPackage in partyPackageInfo.DefaultIfEmpty()
-
-                //                 // Where clause to filter by BranchId and CompanyId
-                //             where customer.BranchId == branchId && customer.CompanyId == companyId
-
-                //             // Select fields
-                //             select new
-                //             {
-                //                 customer.FirstName,
-                //                 customer.LastName,
-                //                 customer.EmailAddress,
-                //                 customer.City,
-                //                 customer.PhoneNo,
-                //                 customer.HonoreeName,
-
-                //                 // State information
-                //                 StateId = state != null ? state.StateId : (int?)null,
-                //                 StateName = state != null ? state.StateName : null,
-
-                //                 // Event information
-                //                 EventDate = eventInfo != null
-                //                            ? (eventInfo.EventInfoEventDate.HasValue
-                //                                ? (DateTime?)eventInfo.EventInfoEventDate.Value.ToDateTime(new TimeOnly(0, 0))
-                //                                : (DateTime?)null)
-                //                            : (DateTime?)null,
-
-                //                 // Contract information
-                //                 ContractNo = timeTeamInfo != null ? timeTeamInfo.ContractNo : null,
-                //                 ContractId = timeTeamInfo != null ? timeTeamInfo.ContractId : null,
-                //                 ContractDate = timeTeamInfo != null ? timeTeamInfo.EntryDate : null,
-
-                //                 // Package information
-                //                 PackageId = contractPackage != null ? contractPackage.PartyPackageId : (int?)null,
-                //                 PackageName = partyPackage != null ? partyPackage.PartyPackageName : null
-                //             }).AsQueryable();
+ 
                 var query = (from customer in _context.CustomerInfos
 
                                  // Left join with ContractEventInfos
@@ -152,11 +90,16 @@ namespace ClownsCRMAPI.Controllers
                              on contractPackageAddon.AddonId equals addon.AddonId into addonInfo
                              from addon in addonInfo.DefaultIfEmpty()
 
+                             join contractBookingPayment in _context.ContractBookingPaymentInfos
+                             on new { CustomerId = (int?)customer.CustomerId, ContractId = (int?)timeTeamInfo.ContractId }
+                             equals new { CustomerId = (int?)contractBookingPayment.CustomerId, ContractId = (int?)contractBookingPayment.ContractId } into contractBookingPayments
+                             from contractBookingPayment in contractBookingPayments.DefaultIfEmpty()
+
                                  // Where clause to filter by BranchId and CompanyId
                              where customer.BranchId == branchId && customer.CompanyId == companyId
 
                              // Group results to aggregate character, bounce, and addon names
-                             group new { customer, eventInfo, timeTeamInfo, state, contractPackage, partyPackage, character, bounce, addon }
+                             group new { customer, eventInfo, timeTeamInfo, state, contractPackage, partyPackage, character, bounce, addon, contractBookingPayment }
                              by new
                              {
                                  customer.FirstName,
@@ -172,7 +115,12 @@ namespace ClownsCRMAPI.Controllers
                                  timeTeamInfo.ContractId,
                                  timeTeamInfo.EntryDate,
                                  contractPackage.PartyPackageId,
-                                 partyPackage.PartyPackageName
+                                 partyPackage.PartyPackageName,
+                                 contractPackage.CategoryId,
+                                 eventInfo.EventInfoVenue,
+                                 contractBookingPayment.PaymentStatusId,
+                                 timeTeamInfo.ContractStatusId
+
                              } into grouped
 
                              // Select the fields and concatenate character, bounce, and addon names
@@ -205,12 +153,23 @@ namespace ClownsCRMAPI.Controllers
 
                                  // Concatenated character names
                                  Characters = string.Join(", ", grouped.Select(g => g.character != null ? g.character.CharacterName : "").Where(name => !string.IsNullOrEmpty(name)).Distinct()),
+                                 CharacterId = string.Join(", ", grouped.Select(g => g.character != null ? g.character.CharacterId.ToString() : "").Distinct().Where(id => !string.IsNullOrEmpty(id))),
 
                                  // Concatenated bounce names
                                  Bounces = string.Join(", ", grouped.Select(g => g.bounce != null ? g.bounce.BounceName : "").Where(name => !string.IsNullOrEmpty(name)).Distinct()),
+                                 BounceId = string.Join(", ", grouped.Select(g => g.bounce != null ? g.bounce.BounceId.ToString() : "").Where(id => !string.IsNullOrEmpty(id)).Distinct()),
 
                                  // Concatenated addon names
-                                 Addons = string.Join(", ", grouped.Select(g => g.addon != null ? g.addon.AddonName : "").Where(name => !string.IsNullOrEmpty(name)).Distinct())
+                                 Addons = string.Join(", ", grouped.Select(g => g.addon != null ? g.addon.AddonName : "").Where(name => !string.IsNullOrEmpty(name)).Distinct()),
+                                 AddonId = string.Join(", ", grouped.Select(g => g.addon != null ? g.addon.AddonId.ToString() : "").Where(id => !string.IsNullOrEmpty(id)).Distinct()),
+
+                                 CategoryId = grouped.Key.CategoryId,
+                                 EventInfoVenue = grouped.Key.EventInfoVenue,
+                                 PaymentStatusId = grouped.Key.PaymentStatusId,
+                                 ContractStatusId = grouped.Key.ContractStatusId,
+                                 Confirmation = grouped.Key.ContractStatusId == 1? true : false,
+                                 Approval = grouped.Key.ContractStatusId == 2? true : false,
+                                 
                              }).AsQueryable();
 
 
@@ -229,6 +188,49 @@ namespace ClownsCRMAPI.Controllers
                 if (!string.IsNullOrEmpty(criteria.CustomerPhone))
                     query = query.Where(x => x.PhoneNo.Contains(criteria.CustomerPhone));
 
+                if (!string.IsNullOrEmpty(criteria.contractNumber))
+                    query = query.Where(x => x.ContractNo.Contains(criteria.contractNumber));
+
+                if (criteria.State !=0 && criteria.State !=null)
+                    query = query.Where(x => x.StateId.Equals(criteria.State));
+
+                if (!string.IsNullOrEmpty(criteria.City))
+                    query = query.Where(x => x.City.Contains(criteria.City));
+
+                if (!string.IsNullOrEmpty(criteria.PrimaryHonoree))
+                    query = query.Where(x => x.HonoreeName.Contains(criteria.PrimaryHonoree));
+
+                if (criteria.Category != 0 && criteria.Category != null)
+                    query = query.Where(x => x.CategoryId.Equals(criteria.Category));
+
+                if (criteria.PartyPackage != 0 && criteria.PartyPackage != null)
+                    query = query.Where(x => x.PackageId.Equals(criteria.PartyPackage));
+
+                if (criteria.Venue != 0 && criteria.Venue != null)
+                    query = query.Where(x => x.EventInfoVenue.Equals(criteria.Venue));
+
+                if (criteria.PaymentStatus != 0 && criteria.PaymentStatus != null)
+                    query = query.Where(x => x.PaymentStatusId.Equals(criteria.PaymentStatus));
+
+
+                query = query.Where(x => x.Approval.Equals(criteria.Approval));
+                query = query.Where(x => x.Confirmation.Equals(criteria.Confirmation));
+
+
+                if (criteria.Characters.HasValue)
+                {
+                    query = query.Where(x => x.CharacterId.Equals(criteria.Characters));
+                }
+
+                if (criteria.Bounces.HasValue)
+                {
+                    query = query.Where(x => x.BounceId.Equals(criteria.Bounces));
+                }
+
+                if (criteria.AddOns.HasValue)
+                {
+                    query = query.Where(x => x.AddonId.Equals(criteria.AddOns));
+                }
                 var test = query.ToList();
                 if (criteria.EventDate.HasValue && criteria.EventDate.ToString() != "1/01/1009 12:00:00 AM")
                 {
